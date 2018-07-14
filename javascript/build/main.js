aa1f0be04962019e9927321f3cb3ea5014c602fa
SmashingQuasar <nicolas.cordier@outlook.com>
2018-07-14 21:40:12 +0200

    First stable build

--- javascript/build/main.js
+++ javascript/build/main.js
@@ -1,19 +1,84 @@
 "use strict";
-var EasyMasonry = /** @class */ (function () {
-    function EasyMasonry(wrapper) {
-        this.wrapper = wrapper;
-        this.baseImages = this.wrapper.querySelectorAll("img");
+var MasonryColumn = (function () {
+    function MasonryColumn(node) {
+        this.height = 0;
+        this.node = node;
     }
-    EasyMasonry.prototype.initialize = function () {
-        console.log(this.baseImages, this.baseImages.length);
-        Array.prototype.forEach.call(this.baseImages, function (baseImage) {
-            console.log(baseImage, baseImage.offsetWidth);
+    MasonryColumn.prototype.getHeight = function () {
+        return this.height;
+    };
+    MasonryColumn.prototype.setHeight = function (height) {
+        this.height = height;
+    };
+    MasonryColumn.prototype.getNode = function () {
+        return this.node;
+    };
+    return MasonryColumn;
+}());
+var EasyMasonry = (function () {
+    function EasyMasonry(root, columns_amount, column_width) {
+        if (column_width === void 0) { column_width = 100 / columns_amount; }
+        var _this = this;
+        this.columnTemplate = document.createElement("masonry-column");
+        this.columns = [];
+        this.root = root;
+        this.elements = this.root.querySelectorAll(":not(masonry-column)");
+        var margins = this.calculateMargins();
+        this.columnTemplate.style.width = "calc(" + column_width + "% - " + margins + "px)";
+        for (var i = 0; i < columns_amount; ++i) {
+            this.columns[i] = new MasonryColumn(this.columnTemplate.cloneNode(true));
+            this.root.appendChild(this.columns[i].node);
+        }
+        Array.prototype.forEach.call(this.elements, function (element) {
+            var smallest_column = _this.getSmallestColumn();
+            _this.columns[smallest_column].node.appendChild(element);
+            _this.columns[smallest_column].setHeight(_this.columns[smallest_column].getHeight() + element.offsetHeight);
         });
+    }
+    EasyMasonry.prototype.calculateMargins = function () {
+        var column_template = window.getComputedStyle(this.columnTemplate);
+        var margin_left = 0;
+        var margin_right = 0;
+        var style;
+        if (column_template.marginRight) {
+            style = column_template.marginRight.match(/[0-9]+/);
+            if (style) {
+                margin_right = +style[0];
+            }
+        }
+        if (column_template.marginLeft) {
+            style = column_template.marginLeft.match(/[0-9]+/);
+            if (style) {
+                margin_left = +style[0];
+            }
+        }
+        return margin_left + margin_right;
+    };
+    EasyMasonry.prototype.getSmallestColumn = function () {
+        var smallest_column;
+        var height = 0;
+        var current_height = 0;
+        for (var i = 0; i < this.columns.length; ++i) {
+            current_height = this.columns[i].getHeight();
+            if (i === 0 || current_height < height) {
+                height = current_height;
+                smallest_column = i;
+            }
+        }
+        return smallest_column;
     };
     return EasyMasonry;
 }());
 window.addEventListener("load", function () {
-    var masonry = new EasyMasonry(document.querySelector("easy-masonry"));
-    masonry.initialize();
-    console.log("test");
+    var wrapper = document.querySelector("easy-masonry");
+    if (wrapper instanceof HTMLElement) {
+        var masonry = new EasyMasonry(wrapper, 4);
+        console.log(masonry);
+    }
+    else if (wrapper !== null) {
+        console.log(wrapper.constructor.name);
+    }
+    else {
+        console.log("EM: Wrapper does not exist.");
+    }
 });
--- javascript/sources/main.ts
+++ javascript/sources/main.ts
@@ -1,34 +1,150 @@
 "use strict";
 
+class MasonryColumn
+{
+    private height: number = 0;
+    readonly node: Node;
+
+    constructor(node: Node)
+    {
+        this.node = node;
+    }
+
+    /**
+     * getHeight
+     */
+    public getHeight(): number
+    {
+        return this.height;
+    }
+
+    /**
+     * setHeight
+     */
+    public setHeight(height: number): void
+    {
+        this.height = height;
+    }
+
+    /**
+     * getNode
+     */
+    public getNode(): Node
+    {
+        return this.node;
+    }
+}
+
 class EasyMasonry
 {
-    private baseImages: NodeList;
-    private wrapper: HTMLElement;
-    
-    constructor(wrapper: HTMLElement)
-    {
-        this.wrapper = wrapper;
-        this.baseImages = this.wrapper.querySelectorAll("img");
-    }
+    private elements: NodeList;
+    private root: HTMLElement;
+    private columnTemplate: HTMLElement = document.createElement("masonry-column");
+    private columns: Array<MasonryColumn> = [];
 
-    initialize()
+    constructor(root: HTMLElement, columns_amount: number, column_width: number = 100/columns_amount)
     {
-        console.log(this.baseImages, this.baseImages.length);
+        this.root = root;
+        this.elements = this.root.querySelectorAll(":not(masonry-column)");
+
+        let margins: number = this.calculateMargins();
+    
+        this.columnTemplate.style.width = `calc(${column_width}% - ${margins}px)`;
+
+        for (let i = 0; i < columns_amount; ++i)
+        {
+            this.columns[i] = new MasonryColumn(this.columnTemplate.cloneNode(true));
+            this.root.appendChild(this.columns[i].node);
+        }
 
         Array.prototype.forEach.call(
-            this.baseImages,
-            (baseImage: HTMLImageElement) => {
-                console.log(baseImage, baseImage.offsetWidth);
+            this.elements,
+            (element: HTMLElement) =>
+            {
+                let smallest_column = this.getSmallestColumn();
+
+                this.columns[smallest_column].node.appendChild(element);
+
+                this.columns[smallest_column].setHeight(this.columns[smallest_column].getHeight() + element.offsetHeight);
+
             }
         );
+    }
+
+    private calculateMargins(): number
+    {
+        let column_template: CSSStyleDeclaration = window.getComputedStyle(this.columnTemplate);
+
+        let margin_left: number = 0;
+        let margin_right: number = 0;
+        let style: RegExpMatchArray | null;
+
+        if (column_template.marginRight)
+        {
+            style = column_template.marginRight.match(/[0-9]+/);
+
+            if (style)
+            {
+                margin_right = +style[0];
+            }
+
+        }
+
+        if (column_template.marginLeft)
+        {
+            style = column_template.marginLeft.match(/[0-9]+/);
+
+            if (style)
+            {
+                margin_left = +style[0];
+            }
+
+        }
+
+        return margin_left + margin_right;
 
     }
+
+    private getSmallestColumn()
+    {
+        let smallest_column: any;
+        let height: number = 0;
+        let current_height: number = 0;
+
+        for (let i = 0; i < this.columns.length; ++i)
+        {
+            current_height = this.columns[i].getHeight();
+
+            if (i === 0 || current_height < height)
+            {
+                height = current_height;
+                smallest_column = i;
+            }
+        }
+
+        return smallest_column;
+    }
+
 }
 window.addEventListener(
     "load",
-    () => {
-        let masonry = new EasyMasonry(document.querySelector("easy-masonry"));
-        masonry.initialize();
-        console.log("test");
+    () =>
+    {
+        let wrapper: HTMLElement | null = document.querySelector("easy-masonry");
+
+        if (wrapper instanceof HTMLElement)
+        {
+            let masonry = new EasyMasonry(wrapper, 4);
+            console.log(masonry);
+        }
+        else if (wrapper !== null)
+        {
+            console.log((<any>wrapper).constructor.name);
+        }
+        else
+        {
+            console.log("EM: Wrapper does not exist.");
+        }
+
     }
 );
